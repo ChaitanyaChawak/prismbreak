bcbe94cc037435fee220ab4623a5f335a086d85b
Chaitanya <chaitanyachawak001@gmail.com>
2024-07-19 17:09:19 +0530

    add class format

--- prismbreak/prismbreak.py
+++ prismbreak/prismbreak.py
@@ -1,69 +1,62 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-import argparse
 import os
 
-def main(args):
-  root = args.root
-  fname = args.fname
-  savedir = args.savedir
-  dpi = args.dpi
-  save_r = f'{savedir}/r_{fname}'
-  save_g = f'{savedir}/g_{fname}'
-  save_b = f'{savedir}/b_{fname}'
+class Prismbreak:
+    def __init__(self, root, fname, savedir=".", dpi=100):
+        self.root = root
+        self.fname = fname
+        self.savedir = savedir
+        self.dpi = dpi
 
-  # Check if the filename ends with .png
-  if not fname.lower().endswith('.png'):
-    raise ValueError("File must have a .png extension.")
+        self.save_r = f'{savedir}/r_{fname}'
+        self.save_g = f'{savedir}/g_{fname}'
+        self.save_b = f'{savedir}/b_{fname}'
 
-  # Check if the file path exists
-  file_path = os.path.join(root, fname)
-  if not os.path.isfile(file_path):
-    raise FileNotFoundError(f"The file {file_path} does not exist.")
+    def splitrgb(self):
 
-  # Open the image
-  img = Image.open(file_path)
+        # Check if the filename ends with .png
+        if not self.fname.lower().endswith('.png'):
+            raise ValueError("File must have a .png extension.")
 
-  # Check if the image size is 0x0
-  if img.size == (0, 0):
-    raise ValueError("The image size is 0x0.")
+        # Check if the file path exists
+        file_path = os.path.join(self.root, self.fname)
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError(f"The file {file_path} does not exist.")
 
-  M = np.asarray(img)
+        # Open the image
+        img = Image.open(file_path)
 
-  # Check if the image has 3 channels
-  if M.ndim != 3 or M.shape[2] != 3:
-    raise ValueError("The image must have 3 channels (RGB).")
+        # Check if the image size is 0x0
+        if img.size == (0, 0):
+            raise ValueError("The image size is 0x0.")
 
-  M = np.asarray(img)
-  height, width, c = M.shape
-  height = height/dpi
-  width = width/dpi
+        M = np.asarray(img)
 
-  fig, ax = plt.subplots(figsize=(width, height), dpi=dpi)
-  ax.imshow(M[:, :, 0], cmap='Reds', vmin=0, vmax=255)
-  ax.set_axis_off()
-  fig.savefig(save_r, bbox_inches='tight', pad_inches=0)
-  print(f'saved {save_r}')
+        # Check if the image has 3 channels
+        if M.ndim != 3 or M.shape[2] != 3:
+            raise ValueError("The image must have 3 channels (RGB).")
 
-  fig, ax = plt.subplots(figsize=(width, height), dpi=dpi)
-  plt.imshow(M[:, :, 1], cmap='Greens', vmin=0, vmax=255)
-  ax.set_axis_off()
-  fig.savefig(save_g, bbox_inches='tight', pad_inches=0)
-  print(f'saved {save_g}')
+        M = np.asarray(img)
+        height, width, c = M.shape
+        height = height/self.dpi
+        width = width/self.dpi
 
-  fig, ax = plt.subplots(figsize=(width, height), dpi=dpi)
-  plt.imshow(M[:, :, 2], cmap='Blues', vmin=0, vmax=255)
-  ax.set_axis_off()
-  fig.savefig(save_b, bbox_inches='tight', pad_inches=0)
-  print(f'saved {save_b}')
+        fig, ax = plt.subplots(figsize=(width, height), dpi=self.dpi)
+        ax.imshow(M[:, :, 0], cmap='Reds', vmin=0, vmax=255)
+        ax.set_axis_off()
+        fig.savefig(self.save_r, bbox_inches='tight', pad_inches=0)
+        print(f'saved {self.save_r}')
 
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Split a PNG image into its RGB components.')
-  parser.add_argument('--root', type=str, required=True, help='Root directory containing the image.')
-  parser.add_argument('--fname', type=str, required=True, help='Filename of the image (e.g., temp.png).')
-  parser.add_argument('--savedir', type=str, required=False, default='.', help='Save destination of the output image.')
-  parser.add_argument('--dpi', type=int, required=False, default=100)
+        fig, ax = plt.subplots(figsize=(width, height), dpi=self.dpi)
+        plt.imshow(M[:, :, 1], cmap='Greens', vmin=0, vmax=255)
+        ax.set_axis_off()
+        fig.savefig(self.save_g, bbox_inches='tight', pad_inches=0)
+        print(f'saved {self.save_g}')
 
-  args = parser.parse_args()
-  main(args)
+        fig, ax = plt.subplots(figsize=(width, height), dpi=self.dpi)
+        plt.imshow(M[:, :, 2], cmap='Blues', vmin=0, vmax=255)
+        ax.set_axis_off()
+        fig.savefig(self.save_b, bbox_inches='tight', pad_inches=0)
+        print(f'saved {self.save_b}')
